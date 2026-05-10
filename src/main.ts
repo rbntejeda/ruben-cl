7677df2db26cf99ba4eeb1bcc330f4b0b3c66192
Ruben Eduardo Tejeda Roa <rubentejedaroa@gmail.com>
2026-05-09 23:58:57 -0400

    fix: load Zone.js for Angular runtime

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { AppComponent } from './app/app.component';
